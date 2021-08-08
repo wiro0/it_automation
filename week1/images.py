fb9dcbd4b59871ee429a22ccf5e0044a20ddb05a
wiro0 <mehlzwerg@gmail.com>
2021-08-09 00:51:38 +0200

    First successful test run.

--- week1/images.py
+++ week1/images.py
@@ -7,17 +7,19 @@ from PIL import Image
 # Store the beginning working directory
 org_dir = os.getcwd()
 
-# Make directory images if it does not exist
+# Make directory "images" if it does not exist
 if not os.path.isdir("/opt/images"):
     os.chdir("/opt")
     os.mkdir("images")
 
-os.chdir(org_dir)
+image_dir = os.path.join(org_dir, "images")
+os.chdir(image_dir)
 res = os.listdir()
 
 # Iterate through all files in the current working directory and manipulate all
 # matching images
 for item in res:
-    if re.search(r".+\.tiff$", item):
-        im = Image(os.path.join(os.getcwd(), item))
+    if item.endswith(".jpg"):
+        print(item)
+        im = Image.open(item)
         im.rotate(90).resize((640,480)).save(os.path.join("/opt/images", item))
