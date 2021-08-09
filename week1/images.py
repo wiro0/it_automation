2530a3181bd842b0b2a57665956f283132d839b0
wiro0 <mehlzwerg@gmail.com>
2021-08-10 01:05:19 +0200

    Added function create_path.
    This function checks, if a directory path exists, and if not, creates it.

--- week1/images.py
+++ week1/images.py
@@ -7,15 +7,28 @@ store the fixed images.
 import os
 from PIL import Image
 
+
+def create_path(new_dir):
+    """
+    Create path/directory for the modified images if it does not exist
+    """
+    dirs_list = new_dir.split("/")
+    branch = "/"
+    for folder in dirs_list:
+        new_branch = os.path.join(branch, folder)
+        if not os.path.isdir(new_branch):
+            os.chdir(branch)
+            os.mkdir(folder)
+        branch = new_branch
+
+    
 # The source image directory
 image_dir = "/home/user/images"
+new_dir = "/opt/icons"
 
 new_extension = ".jpg"
 
-# Create directory for the modified images if it does not exist
-if not os.path.isdir("/opt/icons"):
-    os.chdir("/opt")
-    os.mkdir("icons")
+create_path(new_dir)
 
 # Create a list of all files in the image directory
 os.chdir(image_dir)
