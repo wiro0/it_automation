aacc9adc3f4b8cc659d3984d334cabf228e38fd1
wiro0 <mehlzwerg@gmail.com>
2021-08-09 09:21:35 +0200

    Changed variable names, switched to a fixed image directory.

--- week1/images.py
+++ week1/images.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
-
+#
 import os
 import re
 from PIL import Image
 
-# Store the beginning working directory
-org_dir = os.getcwd()
+# The image directory
+image_dir = "/home/pi/git/it_automation/images"
 
-# Make directory "images" if it does not exist
+# Create directory for the modified images if it does not exist
 if not os.path.isdir("/opt/images"):
     os.chdir("/opt")
     os.mkdir("images")
 
-image_dir = os.path.join(org_dir, "images")
+# Create a list of all files in the image directory
 os.chdir(image_dir)
-res = os.listdir()
+files = os.listdir()
 
 # Iterate through all files in the current working directory and manipulate all
 # matching images
-for item in res:
-    if item.endswith(".jpg"):
+for item in files:
+    if os.isfile(item) and item.endswith(".jpg"):
         print(item)
         im = Image.open(item)
         im.rotate(90).resize((640,480)).save(os.path.join("/opt/images", item))
