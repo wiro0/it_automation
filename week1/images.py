8852afdffa3245529b314d7a5d050a44c9650de3
wiro0 <71041131+wiro0@users.noreply.github.com>
2021-08-09 09:43:40 +0200

    Added a description of the program. Added a variable for the target files extension for easy editing.

--- week1/images.py
+++ week1/images.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-#
+"""
+A program to manipulate images in a directory through the PIL/pillow module and
+store the fixed images.
+"""
+
 import os
 import re
 from PIL import Image
 
-# The image directory
+# The source image directory
 image_dir = "/home/test/images"
+
+# The extension of the target images
+extension = ".jpg"
 
 # Create directory for the modified images if it does not exist
 if not os.path.isdir("/opt/images"):
@@ -14,12 +21,12 @@ if not os.path.isdir("/opt/images"):
 
 # Create a list of all files in the image directory
 os.chdir(image_dir)
-files = os.listdir()
+dir_content = os.listdir()
 
-# Iterate through all files in the current working directory and manipulate all
-# matching images
-for item in files:
-    if os.isfile(item) and item.endswith(".jpg"):
+# Iterate through all files in the image directory and manipulate all files with
+# the matching extension.
+for item in dir_content:
+    if os.isfile(item) and item.endswith(extension):
         print("Processing image:", item)
         im = Image.open(item)
         im.rotate(90).resize((640,480)).save(os.path.join("/opt/images", item))
