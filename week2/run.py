16db6f51005dfbb10e2ef64414691e2d4d9b2af9
wiro0 <mehlzwerg@gmail.com>
2021-08-10 21:21:10 +0200

    Added requests.post method.

--- week2/run.py
+++ week2/run.py
@@ -9,8 +9,20 @@ os.chdir("/data/feedback")
 result = os.listdir()
 
 for item in result:
-    if os.isfile(item):
+    if os.path.isfile(item):
+        print("Processing file:", item)
         with open (item, "r") as file:
             text = file.readlines()
+            data = {}
             for num, line in enumerate(text):
                 data[keys[num]] = line
+        file.close()
+    print("Sending data")
+    for i in range(5):
+        response = requests.post("http://<IP>/feedback", data)
+        if response.status_code == "201":
+            print("Sending data successfull")
+            break
+        else:
+            print("Sending data error.")
+    
